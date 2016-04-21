b391202098f41507d2d93e10f7155a0e073f529b
0x4C4A <Linards.Jukmanis@0x4c4a.com>
2016-04-21 22:49:33 +0300

    Correct clock settings for STM32F072B discovery board + Hello world LED.

--- src/main.c
+++ src/main.c
@@ -78,28 +78,43 @@ int main(void)
   /* Configure the system clock to have a system clock = 48 Mhz */
   SystemClock_Config();
 
-
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   /* Add your application code here
      */
+  //GPIOC->MODER |= 1 << (2*9);
 
+  GPIO_InitTypeDef  GPIO_InitStruct;
+
+  /* Configure the GPIO_LED pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP; /* GPIO_PuPd_DOWN */
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /* Infinite loop */
   while (1)
   {
+    //GPIOC->ODR ^= 1>>9;
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_9);
+    uint32_t i;
+    for(i=0; i<1000000; i++)
+      __asm("NOP");
   }
 }
 
 /**
   * @brief  System Clock Configuration
   *         The system Clock is configured as follow :
-  *            System Clock source            = PLL (HSE)
+  *            System Clock source            = PLL (HSI48)
   *            SYSCLK(Hz)                     = 48000000
   *            HCLK(Hz)                       = 48000000
   *            AHB Prescaler                  = 1
   *            APB1 Prescaler                 = 1
-  *            HSE Frequency(Hz)              = 8000000
-  *            PREDIV                         = 1
-  *            PLLMUL                         = 6
+  *            HSI Frequency(Hz)              = 48000000
+  *            PREDIV                         = 2
+  *            PLLMUL                         = 2
   *            Flash Latency(WS)              = 1
   * @param  None
   * @retval None
@@ -109,19 +124,19 @@ static void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_OscInitTypeDef RCC_OscInitStruct;
 
-  /* Enable HSE Oscillator and Activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  /* Select HSI48 Oscillator as PLL source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI48;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
   {
     Error_Handler();
   }
 
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 clocks dividers */
+  /* Select PLL as system clock source and configure the HCLK and PCLK1 clocks dividers */
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
